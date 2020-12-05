ed3a180eac3b40cb70c715daffd6fa74c320ffad
Nabil <nabil_ee@hotmail.com>
2020-12-05 02:23:12 +0100

    folder structure + header component

---
+++ src/constants.js
@@ -0,0 +1,20 @@
+export const COLORS = {
+  PRIMARY: "#BF953F",
+  SECONDARY: "#c56183",
+  TERTIARY: "#FBF5B7",
+  WHITE: "#FFF",
+  BLACK: "#181819",
+  GREY: "#242124",
+}
+
+export const FONT_FAMILIES = {
+  TITLE: "Oswald",
+  TEXT: "Roboto",
+  BUTTON: "Oswald",
+}
+
+export const MEDIA_QUERIES = {
+  SMALL: "520px",
+  MEDIUM: "980px",
+  LARGE: "1200px",
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,93 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, useStaticQuery, graphql } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
+import { Wrapper, Image, Movie, BottomEdgeDown, BottomEdgeUp } from './pageStyles/pageStyles'
+import { COLORS } from '../constants'
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const {
+    wpcontent: {
+      page: {
+        homeMeta: {
+          homePageHeaderTitle,
+          homePageHeaderDescription,
+          homePageHeaderPicture,
+          homePageDescription,
+          homePageFeaturedMovies,
+        },
+      },
+    },
+  } = useStaticQuery(graphql`
+    query {
+      wpcontent {
+        page(id: "home", idType: URI) {
+          homeMeta {
+            homePageHeaderTitle
+            homePageHeaderDescription
+            homePageHeaderPicture {
+              altText
+              sourceUrl
+              imageFile {
+                childImageSharp {
+                  fluid(quality: 100) {
+                    ...GatsbyImageSharpFluid_withWebp
+                  }
+                }
+              }
+            }
+            homePageDescription
+            homePageFeaturedMovies {
+              ... on WPGraphql_Movie {
+                id
+                movie {
+                  title
+                  description
+                  year
+                  language
+                  director
+                  company
+                  review
+                  cover {
+                    altText
+                    sourceUrl
+                    imageFile {
+                      childImageSharp {
+                        fluid(quality: 100) {
+                          ...GatsbyImageSharpFluid_withWebp
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Wrapper>
+        <div className="banner">
+          <Image fluid={homePageHeaderPicture.imageFile.childImageSharp.fluid} alt={homePageHeaderPicture.altText} />
+          <div className="inner-div">
+            <p className="header-title">{homePageHeaderTitle}</p>
+            <p className="header-description">{homePageHeaderDescription}</p>
+          </div>
+          <BottomEdgeDown color={COLORS.BLACK} />
+        </div>
+        <div className="description">
+          <p>{homePageDescription}</p>
+          <BottomEdgeUp color={COLORS.PRIMARY} />
+        </div>
+      </Wrapper>
+    </Layout>
+  )
+}
 
 export default IndexPage

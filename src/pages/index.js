bd384225cc4abe2d403d47e30158dddf9e8e1175
Nabil <nabil_ee@hotmail.com>
2020-12-05 21:36:13 +0100

    rename components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Link, useStaticQuery, graphql } from "gatsby"
 
-import Layout from "../components/Layout"
-import SEO from "../components/Seo"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
 import { Wrapper, Image, Movie, BottomEdgeDown, BottomEdgeUp } from '../pageStyles/pageStyles'
 import { COLORS } from '../constants'
 
